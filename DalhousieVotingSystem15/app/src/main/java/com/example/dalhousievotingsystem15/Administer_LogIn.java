74730d9bba9bd5e41f309e2fb967d7b24c0abac5
Borong Chen <br548178@dal.ca>
2020-03-12 02:26:03 -0300

    Update Administer_LogIn.java, The admin can set an end date for a vote (Pair: Borong, Xingyu)

--- DalhousieVotingSystem15/app/src/main/java/com/example/dalhousievotingsystem15/Administer_LogIn.java
+++ DalhousieVotingSystem15/app/src/main/java/com/example/dalhousievotingsystem15/Administer_LogIn.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -18,6 +19,12 @@ public class Administer_LogIn extends AppCompatActivity {
     private Button candidateControl;
     private TextView welcomeText;
     private Button logOut;
+    private Button StudentControl;
+    private EditText year;
+    private EditText mouth;
+    private EditText date;
+    private boolean noted;
+    private Button setFinishdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,28 +33,42 @@ public class Administer_LogIn extends AppCompatActivity {
 
         welcomeText=(TextView)findViewById(R.id.Welcome);
         logOut=(Button)findViewById(R.id.LogOut);
+        studentControl=(Button)findViewById(R.id.StudentControl);
+        year=(EditText)findViewById(R.id.edityy);
+        mouth=(EditText)findViewById(R.id.editmm);
+        date=(EditText)findViewById(R.id.editdd);
+        setFinishdate=(Button)findViewById(R.id.setfinsihdate);
+        noted=false;
+        welcomeText.setText("WelCome Administor");
 
 
-        MainActivity.admRef.addValueEventListener(new ValueEventListener() {
+
+
+
+
+
+        setFinishdate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                welcomeText=(TextView)findViewById(R.id.Welcome);
-                String netid=MainActivity.user.netID;
-                String password=dataSnapshot.child(netid).child("Password").getValue(String.class);
-                welcomeText.setText("Password  "+password+"\n"+"Your CSID is: "+netid);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
+            public void onClick(View v) {
+                String finishdate=(year.getText().toString()+"/"+mouth.getText().toString()+"/"+date.getText().toString());
+                MainActivity.rootDatabase.child("FinishDate").setValue(finishdate);
             }
         });
+
+
 
         logOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MainActivity.logout=true;
                 startActivity(new Intent(Administer_LogIn.this, MainActivity.class));
+            }
+        });
+
+        studentControl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Administer_LogIn.this, Administer_StudentControl.class));
             }
         });
 
